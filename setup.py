cec96face3dd213120a6b73c5c481d578f4b5251
Debbie Chong <debbie.chong@monash.edu>
2022-08-09 10:00:05 +1000

    The setup file that will hopefully work

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+	name='MOGONET',
+	version='0.1',
+	description='Multi-omics integration using graph convolution networks',
+	author='Tongxin Wang',
+	url='https://github.com/txWang/MOGONET'
+)
